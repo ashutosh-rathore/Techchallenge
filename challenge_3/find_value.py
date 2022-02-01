ff0103348562519d4d4d9db8d0f6be2fa1ff0f1e
Ashutosh Rathore <ashutosh@Ashutoshs-MacBook-Air.local>
2022-02-01 11:01:47 +0530

    challenge 3

---
+++ challenge_3/find_value.py
@@ -0,0 +1,15 @@
+
+object = {"a":{"b":{"c":"d"}}}
+def get_value(obj,key):
+    keys = []
+    value = {}
+    while type(value) == dict:
+        keys.append(list(obj.keys())[0])
+        value = list(obj.values())[0]
+        obj = list(obj.values())[0]
+    if key in keys:
+        print(value)
+    else:
+        print("Invalid Key")
+ 
+get_value(object,"a")
